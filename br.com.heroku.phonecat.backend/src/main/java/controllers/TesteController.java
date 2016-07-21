414cd4e31707bdc28c787db2003afe33ca5bb7bc
eliasadriano <eliasadriano@192.168.0.101>
2016-07-20 23:40:38 -0300

    first vraptor controller

---
+++ br.com.heroku.phonecat.backend/src/main/java/controllers/TesteController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class TesteController {
+
+	public void teste(){
+		System.out.println("teste vraptor");
+		
+		
+		
+	}
+	
+	
+	
+}
